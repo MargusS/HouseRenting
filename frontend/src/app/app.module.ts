587033fab128db1f189377f6ea82cf44d7d30761
MargusS <agusnicolas21@gmail.com>
2022-09-27 10:27:40 +0100

    Service and model

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { ComponentsModule } from './components/components.module';
 
 @NgModule({
--- frontend/src/app/service/house.service.ts
+++ frontend/src/app/service/house.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { House } from '../models/house';
 
 @Injectable({
   providedIn: 'root'
@@ -13,4 +14,20 @@ export class HouseService {
   getHouses() {
     return this.httpClient.get(this.path + '/lista');
   }
+  getDetail(id:number) {
+    return this.httpClient.get(this.path + `/detail/${id}`);
+  }
+
+  postCreate(house: House){
+    return this.httpClient.post(this.path + `/create`, house);
+  }
+
+  putUpdate(id:number,house:House){
+    return this.httpClient.put(this.path + `/update/${id}`,house);
+  }
+
+  deleteHouse(id:number){
+    return this.httpClient.delete(this.path + `/delete/${id}`);
+  }
+
 }
